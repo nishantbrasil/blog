cd7349b56abedf0a95990e3a250e63370b2faa24
nishant <nishantbrasil@gmail.com>
2017-07-23 00:34:45 +0530

    Added Support for login and logout

--- content/views.py
+++ content/views.py
@@ -5,7 +5,7 @@ from .forms import PostForm
 # Create your views here.
 def post_list(request):
 	template_name = 'post_list.html'
-	post = Post.objects.all().order_by('created_date')
+	post = Post.objects.all().order_by('-created_date')
 	context = {'Post' : post}
 	print(context)
 	return render(request, template_name, context)
