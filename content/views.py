59afa6306bd9d83f19a7234ed7e2662b6249e247
nishant <nishantbrasil@gmail.com>
2017-07-22 10:47:28 +0530

    Added Html

--- content/views.py
+++ content/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+	template_name = 'post_list.html'
+	context = {}
+	return render(request, template_name, context)
