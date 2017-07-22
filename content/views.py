050403d90884689b81627c758037f372d701a52e
nishant <nishantbrasil@gmail.com>
2017-07-22 11:18:42 +0530

    AddedHtml

--- content/views.py
+++ content/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def post_list(request):
 	template_name = 'post_list.html'
-	context = {}
+	post = Post.objects.all().order_by('created_date')
+	context = {'Post' : post}
+	print(context)
 	return render(request, template_name, context)
